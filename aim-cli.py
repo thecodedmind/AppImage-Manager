ad38407bb37093f61c40e87bd80586f0676cf39f
Kai <34060097+Kaiz0r@users.noreply.github.com>
2018-10-24 11:39:33 +0100

    Add files via upload

--- aim-cli.py
+++ aim-cli.py
@@ -72,6 +72,8 @@ def process_command(command):
 		print("install: Checks the download directory for appimages and moves them to the apps directory.\n")
 		print("run <appname>: Runs the app from the apps directory. Only partial name is required, e.i. run yout > this will find any file with yout in the name, like youtube.\n")
 		print("find <optional appname>: Searches app directory for apps. If no name is supplied, lists all. If a name is given, will find all apps with a matching name.")
+		print("set <key> <value>: Sets the config variables.")
+		print("get <key>: Shows the config value.")
 		print("delete <appname>: Deletes the app from the apps directory. Only partial name is required.\n")
 		print("help: This help menu... not that you needed this help menu to tell you that it was a help menu, but thats just a thing help menus seem to do. Hi, I'm a help menu, I'm here to help. But I can't help you with help, I can only help those who help themself...... help me.")
 		print("===========================")
@@ -104,6 +106,7 @@ def install():
 	
 def find(appname = ""):
 	pth = getConfig()['apps_path']
+	print("=======================================")
 	if appname == "":
 		for file in os.listdir(pth):
 			filename = os.fsdecode(file)
@@ -114,7 +117,8 @@ def find(appname = ""):
 			filename = os.fsdecode(file)
 			if filename.lower().endswith(".appimage") and appname.lower() in filename.lower():
 				print(filename)
-			
+	print("=======================================")
+	
 def run(app):
 	pth = getConfig()['apps_path']
 	print(f"Runing {app}")
@@ -128,10 +132,12 @@ def run(app):
 				break
 	try:
 		subprocess.Popen(getConfig()['apps_path']+app)
+			
 	except PermissionError:
 		print(f"Setting executable permissions for {app}...")
 		subprocess.run(['chmod', 'u+x', getConfig()['apps_path']+app])
 		subprocess.Popen(getConfig()['apps_path']+app)
+		
 	except Exception as e:
 		print(e)
 		
@@ -145,10 +151,16 @@ if __name__ == "__main__":
 	if len(command) > 0:
 		process_command(command)
 	else:
+		subprocess.run(['clear'])
 		while True:
-			print("AppImage Manager\nInteractive Mode. ('exit' or 'quit' to close.)")
+			print("AppImage Manager\nInteractive Mode. ('exit' or 'quit' to close. Clear the terminal text with 'clear'.)")
 			f = input("Input command: ")
 			command = f.split(" ")
 			if command[0] == "exit" or command[0] == "quit":
 				break
+			if command[0] == "clear":
+				subprocess.run(['clear'])
+			
 			process_command(command)
+			if command[0] == "run" and getConfig()['cli_quit_when_run'].lower() == "true":
+				break
